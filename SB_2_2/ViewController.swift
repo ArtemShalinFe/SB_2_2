bf2f8ddad86b6db552094c510a5da7ea738ae89d
Artem Shalin <a.shalin@rtinform.ru>
2022-01-10 22:20:05 +0300

    add logic

--- SB_2_2/ViewController.swift
+++ SB_2_2/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var areaColor: UIView!
+    
+    @IBOutlet var labelRedValue: UILabel!
+    @IBOutlet var sliderRedValue: UISlider!
+    
+    @IBOutlet var labelGreenValue: UILabel!
+    @IBOutlet var sliderGreenValue: UISlider!
+    
+    @IBOutlet var labelBlueValue: UILabel!
+    @IBOutlet var sliderBlueValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        areaColor.layer.cornerRadius = 20
+        
+        updateAreaBackgroundColor()
     }
 
-
+    @IBAction func changeRedValue() {
+        labelRedValue.text = String(sliderRedValue.value)
+        updateAreaBackgroundColor()
+    }
+    
+    @IBAction func changeGreenValue() {
+        labelGreenValue.text = String(sliderGreenValue.value)
+        updateAreaBackgroundColor()
+    }
+    
+    @IBAction func changeBlueValue() {
+        labelBlueValue.text = String(sliderBlueValue.value)
+        updateAreaBackgroundColor()
+    }
+        
+    func updateAreaBackgroundColor() {
+        
+        let newColor = CIColor(red: CGFloat(sliderRedValue.value),      green: CGFloat(sliderGreenValue.value),
+            blue: CGFloat(sliderBlueValue.value)
+        )
+        areaColor.backgroundColor = UIColor(ciColor: newColor)
+        
+    }
+    
 }
-
